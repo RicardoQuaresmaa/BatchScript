7e71d2d8640581bfe5f501d9c88e218ddb71d81f
eymen alagöz <eymen_15@hotmail.com>
2018-01-02 14:30:52 +0300

    q7

---
+++ network-ping.bat
@@ -0,0 +1,59 @@
+@echo off 
+cd C:\Users\bny\Desktop
+ping -n 1 192.168.37.10>> pinggg.txt
+ping -n 1 192.168.37.11>> pinggg.txt
+ping -n 1 192.168.37.12>> pinggg.txt
+ping -n 1 192.168.37.13>> pinggg.txt
+ping -n 1 192.168.37.14>> pinggg.txt
+ping -n 1 192.168.37.16>> pinggg.txt
+ping -n 1 192.168.37.17>> pinggg.txt
+ping -n 1 192.168.37.18>> pinggg.txt
+ping -n 1 192.168.37.19>> pinggg.txt
+ping -n 1 192.168.37.20>> pinggg.txt
+ping -n 1 192.168.37.203>> pinggg.txt
+ping -n 1 192.168.37.204>> pinggg.txt
+ping -n 1 192.168.37.205>> pinggg.txt
+ping -n 1 192.168.37.206>> pinggg.txt
+ping -n 1 192.168.37.207>> pinggg.txt
+ping -n 1 192.168.37.21>> pinggg.txt
+ping -n 1 192.168.37.22>> pinggg.txt
+ping -n 1 192.168.37.23>> pinggg.txt
+ping -n 1 192.168.37.24>> pinggg.txt
+ping -n 1 192.168.37.25>> pinggg.txt
+ping -n 1 192.168.37.26>> pinggg.txt
+ping -n 1 192.168.37.27>> pinggg.txt
+ping -n 1 192.168.37.28>> pinggg.txt
+ping -n 1 192.168.37.29>> pinggg.txt
+ping -n 1 192.168.37.40>> pinggg.txt
+ping -n 1 192.168.37.41>> pinggg.txt
+ping -n 1 192.168.37.42>> pinggg.txt
+ping -n 1 192.168.37.43>> pinggg.txt
+ping -n 1 192.168.37.44>> pinggg.txt
+ping -n 1 192.168.37.45>> pinggg.txt
+ping -n 1 192.168.37.46>> pinggg.txt
+ping -n 1 192.168.37.47>> pinggg.txt
+ping -n 1 192.168.37.48>> pinggg.txt
+ping -n 1 192.168.37.50 >> pinggg.txt
+ping -n 1 192.168.37.51>> pinggg.txt
+ping -n 1 192.168.37.52>> pinggg.txt
+ping -n 1 192.168.38.17>> pinggg.txt
+ping -n 1 192.168.38.18>> pinggg.txt
+ping -n 1 192.168.38.19>> pinggg.txt
+ping -n 1 192.168.38.33>> pinggg.txt
+ping -n 1 192.168.38.34>> pinggg.txt
+ping -n 1 192.168.38.49>> pinggg.txt
+ping -n 1 192.168.38.50>> pinggg.txt
+ping -n 1 192.168.38.51>> pinggg.txt
+ping -n 1 192.168.38.65>> pinggg.txt
+ping -n 1 192.168.38.66>> pinggg.txt
+ping -n 1 192.168.38.67>> pinggg.txt
+ping -n 1 192.168.39.17>> pinggg.txt
+ping -n 1 192.168.39.33>> pinggg.txt
+ping -n 1 192.168.39.49>> pinggg.txt
+ping -n 1 192.168.39.65>> pinggg.txt
+ping -n 1 192.168.39.81>> pinggg.txt
+ping -n 1 192.168.39.97>> pinggg.txt
+ping -n 1 192.168.38.129>> pinggg.txt
+ping -n 1 192.168.39.129>> pinggg.txt
+
+echo "The program has completed" >> pinggg.txt
